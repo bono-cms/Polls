b3b30235b88a1264cdb5d784577a4a6746ae16a7
David Yang <daworld.ny@gmail.com>
2015-10-15 21:26:06 +0500

    Renamed langId to lang_id

--- Storage/MySQL/schema.sql
+++ Storage/MySQL/schema.sql
@@ -4,7 +4,7 @@ DROP TABLE IF EXISTS `bono_module_polls_answers`;
 CREATE TABLE `bono_module_polls_answers` (
 	
 	`id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	`langId` INT NOT NULL,
+	`lang_id` INT NOT NULL,
 	`categoryId` INT NOT NULL,
 	`order` INT NOT NULL,
 	`published` varchar(1) NOT NULL,
@@ -17,7 +17,7 @@ DROP TABLE IF EXISTS `bono_module_polls_categories`;
 CREATE TABLE `bono_module_polls_categories` (
 
 	`id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	`langId` INT NOT NULL,
+	`lang_id` INT NOT NULL,
 	`name` varchar(254) NOT NULL
 	
 ) DEFAULT CHARSET = UTF8;
