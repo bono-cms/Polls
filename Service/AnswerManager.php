c76e22ee6c6b17c94800ad7eff550c72a8a7419c
David Yang <daworld.ny@gmail.com>
2016-01-16 21:58:27 +0500

    Removed extra line

--- Service/AnswerManager.php
+++ Service/AnswerManager.php
@@ -159,7 +159,7 @@ final class AnswerManager extends AbstractManager
     {
         return $this->answerMapper->deleteById($id);
     }
-    
+
     /**
      * Deletes a collection of answers by their associated ids
      * 
