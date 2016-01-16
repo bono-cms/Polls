c5aa6dcb9f0e228e5e1509eea68209490e8a6d28
David Yang <daworld.ny@gmail.com>
2016-01-16 21:10:52 +0500

    Added missing contract definitions

--- Storage/AnswerMapperInterface.php
+++ Storage/AnswerMapperInterface.php
@@ -13,4 +13,78 @@ namespace Polls\Storage;
 
 interface AnswerMapperInterface
 {
+    /**
+     * Gets amount of voting options by associated category id
+     * 
+     * @param string $categoryId
+     * @return integer
+     */
+    public function countByCategoryId($categoryId);
+
+    /**
+     * Updates sorting order
+     * 
+     * @param string $id
+     * @param string $order
+     * @return boolean
+     */
+    public function updateOrder($id, $order);
+
+    /**
+     * Updates published state
+     * 
+     * @param string $id
+     * @param string $state
+     * @return boolean
+     */
+    public function updatePublished($id, $state);
+
+    /**
+     * Fetches all answers by associated category id
+     * 
+     * @param string $categoryId
+     * @param boolean $published Whether to filter by published only
+     * @return array
+     */
+    public function fetchAllByCategoryId($categoryId, $published);
+
+    /**
+     * Adds new answer
+     * 
+     * @param array $data
+     * @return boolean
+     */
+    public function insert(array $data);
+
+    /**
+     * Updates an answer
+     * 
+     * @param array $data
+     * @return boolean
+     */
+    public function update(array $data);
+
+    /**
+     * Fetches answer's data by its associated id
+     * 
+     * @param string $id
+     * @return array
+     */
+    public function fetchById($id);
+
+    /**
+     * Deletes all answers associated with particular category id
+     * 
+     * @param string $id Category id
+     * @return boolean
+     */
+    public function deleteAllByCategoryId($id);
+
+    /**
+     * Deletes an answer by its associated id
+     * 
+     * @param string $id
+     * @return boolean
+     */
+    public function deleteById($id);
 }
