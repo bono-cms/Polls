9c1628421e3f962b25af18c220a9f7ca3b5f33a2
David Yang <daworld.ny@gmail.com>
2016-04-30 22:37:20 +0500

    Replaced raw path with route

--- Config/module.config.php
+++ Config/module.config.php
@@ -12,7 +12,7 @@
 return array(
     'module' => array(
         'name'  => 'Polls',
-        'url'   => '/admin/module/polls',
+        'route'  => 'Polls:Admin:Browser@indexAction',
         'icon'  => 'fa fa-star-o fa-5x',
         'order' => 1,
         'description' => 'Polls module lets you to easily manage polls on your site',
