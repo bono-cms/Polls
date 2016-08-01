4c215e70f1439ecb63c64fe9b957fdcc7d8e3a2a
David Yang <daworld.ny@gmail.com>
2016-08-01 22:29:05 +0500

    Removed extras from module configuration file

--- Config/module.config.php
+++ Config/module.config.php
@@ -1,20 +1,9 @@
 <?php
 
-/**
- * This file is part of the Bono CMS
- * 
- * Copyright (c) No Global State Lab
- * 
- * For the full copyright and license information, please view
- * the license file that was distributed with this source code.
- */
-
 return array(
-    'module' => array(
-        'name'  => 'Polls',
-        'route'  => 'Polls:Admin:Browser@indexAction',
-        'icon'  => 'fa fa-star-o fa-5x',
-        'order' => 1,
-        'description' => 'Polls module lets you to easily manage polls on your site',
-    )
+ 'name'  => 'Polls',
+ 'route'  => 'Polls:Admin:Browser@indexAction',
+ 'icon'  => 'fa fa-star-o fa-5x',
+ 'order' => 1,
+ 'description' => 'Polls module lets you to easily manage polls on your site',
 );
