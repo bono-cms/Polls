86dd91472ee958f51b2fe830225c2492cb8a2537
David Yang <daworld.ny@gmail.com>
2016-11-01 00:18:22 +0500

    Updated empty controller

--- Controller/Vote.php
+++ Controller/Vote.php
@@ -1,12 +1,36 @@
 <?php
 
+/**
+ * This file is part of the Bono CMS
+ * 
+ * Copyright (c) No Global State Lab
+ * 
+ * For the full copyright and license information, please view
+ * the license file that was distributed with this source code.
+ */
 
-class Vote
+namespace Polls\Controller;
+
+use Site\Controller\AbstractController;
+
+final class Vote extends AbstractController
 {
     /**
+     * Votes for an answer
+     * 
      * @return string
      */
     public function voteAction()
+    {
+        
+    }
+
+    /**
+     * View results
+     * 
+     * @return string
+     */
+    public function resultsAction()
     {
         
     }
