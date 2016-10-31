e13af634d53e264255672d1fa12572162e92b5e6
David Yang <daworld.ny@gmail.com>
2016-11-01 00:02:43 +0500

    Replaced icon with more compliant

--- Config/module.config.php
+++ Config/module.config.php
@@ -4,7 +4,7 @@ return array(
  'name'  => 'Polls',
  'caption'  => 'Polls',
  'route'  => 'Polls:Admin:Browser@indexAction',
- 'icon'  => 'fa fa-star-o fa-5x',
+ 'icon'  => 'fa fa-bar-chart fa-5x',
  'order' => 1,
  'description' => 'Polls module lets you to easily manage polls on your site',
 );
