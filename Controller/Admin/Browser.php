e9655e9dc36cd4a89518be8236275b48f71692a7
David Yang <daworld.ny@gmail.com>
2016-12-16 22:53:18 +0500

    Persists category id

--- Controller/Admin/Browser.php
+++ Controller/Admin/Browser.php
@@ -12,9 +12,26 @@
 namespace Polls\Controller\Admin;
 
 use Cms\Controller\Admin\AbstractController;
+use Krystal\Form\Gadget\LastCategoryKeeper;
 
 final class Browser extends AbstractController
 {
+    /**
+     * Returns category keeper service
+     * 
+     * @return \Krystal\Form\Gadget\LastCategoryKeeper
+     */
+    private function getCategoryIdKeeper()
+    {
+        static $keeper = null;
+
+        if (is_null($keeper)) {
+            $keeper = new LastCategoryKeeper($this->sessionBag, 'last_poll_category_id');
+        }
+
+        return $keeper;
+    }
+
     /**
      * Display grids
      * 
@@ -32,7 +49,16 @@ final class Browser extends AbstractController
 
         // If no explicit id specified, then use the latest
         if (is_null($categoryId)) {
-            $categoryId = $categoryManager->getLastId();
+            // Get the id, if not specified explicitly
+            if ($this->getCategoryIdKeeper()->hasLastCategoryId()) {
+                $categoryId = $this->getCategoryIdKeeper()->getLastCategoryId();
+            } else {
+                $categoryId = $categoryManager->getLastId();
+            }
+
+        } else {
+            // Persists if specified
+            $this->getCategoryIdKeeper()->persistLastCategoryId($categoryId);
         }
 
         return $this->view->render('browser', array(
