f6a6c2c5914823e43d18dec9d9772482f23cb50b
David Yang <daworld.ny@gmail.com>
2016-12-18 10:59:46 +0500

    Boostrap shared $polls service

---
+++ Service/SiteBootstrapper.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * This file is part of the Bono CMS
+ * 
+ * Copyright (c) No Global State Lab
+ * 
+ * For the full copyright and license information, please view
+ * the license file that was distributed with this source code.
+ */
+
+namespace Slider\Service;
+
+use Cms\Service\AbstractSiteBootstrapper;
+
+final class SiteBootstrapper extends AbstractSiteBootstrapper
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function bootstrap()
+    {
+        $this->view->addVariable('polls', $this->moduleManager->getModule('Polls')->getService('siteService'));
+    }
+}
