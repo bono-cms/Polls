f49af5103895328688f87d3d7d7bb13639d5d44e
dave-yang <daworld.ny@gmail.com>
2018-12-17 19:45:45 +0500

    Minor fixes. Use shared action partial

--- Config/module.config.php
+++ Config/module.config.php
@@ -4,7 +4,6 @@ return array(
  'name'  => 'Polls',
  'caption'  => 'Polls',
  'route'  => 'Polls:Admin:Browser@indexAction',
- 'icon'  => 'fa fa-bar-chart fa-5x',
  'order' => 1,
  'description' => 'Polls module lets you to easily manage polls on your site',
 );
