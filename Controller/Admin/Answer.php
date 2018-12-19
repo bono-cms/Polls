66690d744dc4e215c1c186de6b9dd7f0b49a6b55
dave-yang <daworld.ny@gmail.com>
2018-12-19 18:42:06 +0500

    Use Grid component to generate table.

--- Controller/Admin/Answer.php
+++ Controller/Admin/Answer.php
@@ -93,8 +93,8 @@ final class Answer extends AbstractController
         $service = $this->getModuleService('answerManager');
 
         // Batch removal
-        if ($this->request->hasPost('toDelete')) {
-            $ids = array_keys($this->request->getPost('toDelete'));
+        if ($this->request->hasPost('batch')) {
+            $ids = array_keys($this->request->getPost('batch'));
 
             $service->deleteByIds($ids);
             $this->flashBag->set('success', 'Selected elements have been removed successfully');
