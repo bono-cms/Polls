097fedeb15e2e2c64843db6f850f1310ef13a97f
dave-yang <daworld.ny@gmail.com>
2019-08-24 17:44:26 +0500

    Fixes in schema

--- Storage/MySQL/schema.sql
+++ Storage/MySQL/schema.sql
@@ -1,20 +1,34 @@
 
+/* Categories */
+DROP TABLE IF EXISTS `bono_module_polls_categories`;
+CREATE TABLE `bono_module_polls_categories` (
+	`id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	`lang_id` INT NOT NULL,
+	`name` varchar(254) NOT NULL,
+    `active` varchar(1) NOT NULL,
+    `class` varchar(50) NOT NULL,
+
+    FOREIGN KEY (lang_id) REFERENCES bono_module_cms_languages(id) ON DELETE CASCADE
+) ENGINE = InnoDB DEFAULT CHARSET = UTF8;
+
+
 DROP TABLE IF EXISTS `bono_module_polls_answers`;
 CREATE TABLE `bono_module_polls_answers` (
-    
     `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	`lang_id` INT NOT NULL,
 	`category_id` INT NOT NULL COMMENT 'Associciated category id',
 	`title` varchar(50) NOT NULL COMMENT 'Answer title`',
 	`order` INT NOT NULL COMMENT 'Sorting order',
 	`published` varchar(1) NOT NULL COMMENT 'Boolean value indicated published state',
-    `votes` INT NOT NULL COMMENT 'Vote count'
-	
+    `votes` INT NOT NULL COMMENT 'Vote count',
+
+    FOREIGN KEY (lang_id) REFERENCES bono_module_cms_languages(id) ON DELETE CASCADE,
+    FOREIGN KEY (category_id) REFERENCES bono_module_polls_categories(id) ON DELETE CASCADE
+
 ) DEFAULT CHARSET=UTF8;
 
 DROP TABLE IF EXISTS `bono_module_polls_web_page_answers`;
 CREATE TABLE `bono_module_polls_web_page_answers` (
-
     `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `lang_id` INT NOT NULL,
     `web_page_id` INT NOT NULL COMMENT 'Attached Web Page ID',
@@ -25,50 +39,33 @@ CREATE TABLE `bono_module_polls_web_page_answers` (
 
     FOREIGN KEY (lang_id) REFERENCES bono_module_cms_languages(id) ON DELETE CASCADE,
     FOREIGN KEY (web_page_id) REFERENCES bono_module_cms_webpages(id) ON DELETE CASCADE
-
 ) DEFAULT CHARSET=UTF8 ENGINE = InnoDB;
 
 
 DROP TABLE IF EXISTS `bono_module_polls_web_page_votes`;
 CREATE TABLE `bono_module_polls_web_page_votes` (
-
     `web_page_id` INT NOT NULL COMMENT 'Attached Web Page ID',
     `user_ip` varchar(30) NOT NULL,
 
     FOREIGN KEY (web_page_id) REFERENCES bono_module_cms_webpages(id) ON DELETE CASCADE
-
 ) DEFAULT CHARSET=UTF8 ENGINE = InnoDB;
 
 DROP TABLE IF EXISTS `bono_module_polls_web_page_settings`;
 CREATE TABLE `bono_module_polls_web_page_settings` (
-
     `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `web_page_id` INT NOT NULL COMMENT 'Attached Web Page ID',
     `lang_id` INT NOT NULL COMMENT 'Attached language ID',
     `published` BOOLEAN NOT NULL COMMENT 'Whether this poll on the webpage is enabled',
     `name` varchar(255) NOT NULL COMMENT 'Poll name',
 
-    FOREIGN KEY (lang_id) REFERENCES bono_module_cms_languages(id) ON DELETE CASCADE,
-    FOREIGN KEY (web_page_id) REFERENCES bono_module_cms_webpages(id) ON DELETE CASCADE
-
+    FOREIGN KEY (web_page_id) REFERENCES bono_module_cms_webpages(id) ON DELETE CASCADE,
+    FOREIGN KEY (lang_id) REFERENCES bono_module_cms_languages(id) ON DELETE CASCADE
 ) DEFAULT CHARSET=UTF8 ENGINE = InnoDB;
 
 DROP TABLE IF EXISTS `bono_module_polls_votes`;
 CREATE TABLE `bono_module_polls_votes` (
-
     `category_id` INT NOT NULL,
-    `user_ip` varchar(30) NOT NULL
+    `user_ip` varchar(30) NOT NULL,
 
-) DEFAULT CHARSET=UTF8;
-
-
-DROP TABLE IF EXISTS `bono_module_polls_categories`;
-CREATE TABLE `bono_module_polls_categories` (
-
-	`id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	`lang_id` INT NOT NULL,
-	`name` varchar(254) NOT NULL,
-    `active` varchar(1) NOT NULL,
-    `class` varchar(50) NOT NULL
-	
-) DEFAULT CHARSET = UTF8;
+    FOREIGN KEY (category_id) REFERENCES bono_module_polls_categories(id) ON DELETE CASCADE
+) ENGINE = InnoDB DEFAULT CHARSET = UTF8;
